37bc855323e9c1b98491bb4a5d5de5cffb4a7e4d
ibnu choldun <ibnucholdun023@gmail.com>
2023-09-04 12:29:48 +0700

    Link & Navigation with useRouter (push) from next/router

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 type Props = {};
 
 const LoginPage = (props: Props) => {
+  // Navigasi menggunakan useRouter yang fungsinya dapat meredirect ke halaman tertentu menggunakan keyword push
+  // penerapannya bisa digunakan pada saat falidasi login dan digunakan pada button login untuk meredirect ke halaman utamanya
+  const { push } = useRouter();
+
+  const handleLogin = () => {
+    push("/product");
+  };
+
   return (
     <div>
       <h1>LoginPage</h1>
+      <button onClick={() => handleLogin()}>Login</button>
       <p>
         Belum punya akun? Daftar <Link href={"/auth/register"}>disini</Link>
       </p>
--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 
 type Props = {};
 
 const ProductPage = (props: Props) => {
+  const [isLogin, setIsLogin] = useState(false);
+  const { push } = useRouter();
+  useEffect(() => {
+    if (!isLogin) {
+      push("/auth/login");
+    }
+  }, []);
   return <div>ProductPage</div>;
 };
 
