ce08f168041b2dbd3c837d79b608e4433ccb61c9
ibnu choldun <ibnucholdun023@gmail.com>
2023-09-05 14:10:40 +0700

    Clent Side Rendering with SWR (useSWR)

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,6 +1,8 @@
+import { fetcher } from "@/utils/swr/fetcher";
 import ProductView from "@/views/Product";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+import useSWR from "swr";
 
 const ProductPage = () => {
   const [isLogin, setIsLogin] = useState(true);
@@ -13,17 +15,24 @@ const ProductPage = () => {
     }
   }, []);
 
-  useEffect(() => {
-    fetch("/api/product").then((res) => {
-      res.json().then((data) => {
-        setProducts(data.data);
-      });
-    });
-  }, []);
+  // useEffect(() => {
+  //   fetch("/api/product").then((res) => {
+  //     res.json().then((data) => {
+  //       setProducts(data.data);
+  //     });
+  //   });
+  // }, []);
+
+  // fetching data menggunakan swr
+  const { data, error, isLoading } = useSWR("/api/product", fetcher);
 
   return (
     <>
-      <ProductView products={products}></ProductView>
+      {/* Fetching menggunakan useEffect dan fetch() */}
+      {/* <ProductView products={products}></ProductView> */}
+
+      {/* Fetching menggunakan useSWR */}
+      <ProductView products={isLoading ? [] : data.data}></ProductView>
     </>
   );
 };
