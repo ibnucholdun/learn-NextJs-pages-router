c157f005b2aeb166148f545035424b086bd1be81
ibnu choldun <ibnucholdun023@gmail.com>
2023-09-06 12:39:10 +0700

    Rendering Dynamic Routes with Server Side Rendering

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -4,20 +4,48 @@ import { useRouter } from "next/router";
 import React from "react";
 import useSWR from "swr";
 
-type Props = {};
+type productType = {
+  id: number;
+  name: string;
+  price: number;
+  category: string;
+  image: string;
+};
 
-const DetailProductPage = (props: Props) => {
+const DetailProductPage = ({ product }: { product: productType }) => {
   const { query } = useRouter();
 
-  const { data, error, isLoading } = useSWR(
-    `/api/product/${query.id}`,
-    fetcher
-  );
+  // clientside
+  // const { data, error, isLoading } = useSWR(
+  //   `/api/product/${query.id}`,
+  //   fetcher
+  // );
   return (
     <div>
-      <DetailProductView product={isLoading ? [] : data.data} />
+      {/* Clientside */}
+      {/* <DetailProductView product={isLoading ? [] : data.data} /> */}
+
+      {/* Serverside */}
+      <DetailProductView product={product} />
     </div>
   );
 };
 
 export default DetailProductPage;
+
+// Serverside
+export const getServerSideProps = async ({
+  params,
+}: {
+  params: { id: string };
+}) => {
+  // fetch data
+  const res = await fetch(`http://localhost:3000/api/product/${params.id}`);
+  const data = await res.json();
+
+  return {
+    props: {
+      product: data.data,
+    },
+  };
+};
